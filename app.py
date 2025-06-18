987707dbd11096d4d62d900fe690cf51af60d36d
Igor Bruno <igorbrunojucurutu@gmail.com>
2025-06-18 10:07:43 -0300

    definição das rotas de login e da pagina de cadastro de categoria

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from models import Funcionario, db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta_aqui'  # Necessário para usar flash()
+app.secret_key = 'sua_chave_secreta_aqui'  
 
-# Configuração do banco
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Inicializa o banco com o app
+
 db.init_app(app)
 
-# Cria as tabelas se não existirem
+
 with app.app_context():
     db.create_all()
 
@@ -30,11 +30,9 @@ def cadastro():
             cpf = request.form['cpf']
             especialidade = request.form['especialidade']
             senha = request.form['senha']
-            telefone = request.form.get('telefone', '')  # opcional
 
             if Funcionario.query.filter_by(email=email).first():
                 print('Este e-mail já está cadastrado.', 'danger')
-                flash('Este e-mail já está cadastrado.', 'danger')
                 return redirect(url_for('cadastro'))
 
             if Funcionario.query.filter_by(cpf=cpf).first():
@@ -49,7 +47,6 @@ def cadastro():
                 email=email,
                 senha=senha_hash,
                 cpf=cpf,
-                telefone=telefone,
                 especialidade=especialidade
             )
             
@@ -65,30 +62,38 @@ def cadastro():
 
     return render_template('cadastro.html')
 
-@app.route('/login', methods=["GET","POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        email = request.form['email']
-        senha = request.form['senha']
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
 
         funcionario = Funcionario.query.filter_by(email=email).first()
 
-        if funcionario and check_password_hash(funcionario.senha, senha):
-            session['usuario_id'] = funcionario.id_funcionario
-            session['usuario_nome'] = funcionario.nome
-            flash('Login realizado com sucesso', 'success')
+        if funcionario:
+            print(f"Hash salvo: {funcionario.senha}")
+            if check_password_hash(funcionario.senha, senha):
+                session['usuario_id'] = funcionario.id_funcionario
+                session['usuario_nome'] = funcionario.nome
+                flash('Login realizado com sucesso', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Senha incorreta.', 'danger')
         else:
-            flash('Email ou senha encorreto, por favor tente novamente', 'danger')
-            return(redirect(url_for('login')))
-        
+            flash('Email não encontrado.', 'danger')
+
+        return redirect(url_for('login'))
+
     return render_template("login.html")
+
 
 @app.route('/dashboard')
 def dashboard():
     if 'usuario_id' not in session:
         flash('Não sei como você veio parar aqui, mas sei que você deve sair', 'warning')
         return redirect(url_for('login'))
-    return f"Bem-vindo {session['usuario_nome']}"
+    return f"Bem-vindo(a) {session['usuario_nome']}"
 
 if __name__ == '__main__':
     app.run(debug=True)
